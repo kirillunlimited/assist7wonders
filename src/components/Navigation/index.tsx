a8cb91edf6e45efd3014c0f35e19388b9ded2733
Kirill Ivanov <kirillunlimited@gmail.com>
2020-11-24 22:22:03 +0300

    Redirect to home page if current route is unavailable

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,17 +4,41 @@ import ROUTES from './../../routes';
 import {IRoutes, IAddons} from "../../types";
 import { useLocation } from 'react-router-dom'
 import {AppBar, Tabs, Tab} from '@material-ui/core';
+import {Redirect} from "react-router-dom";
+import {useState, useEffect} from "react";
 
 interface IProps {
 	addons: IAddons;
 }
 
+const DEFAULT_ROUTE = '/';
+
 export default function Navigation(props: IProps) {
 	const location = useLocation();
+	const [isRouteAvailable, setIsRouteAvailable] = useState(true);
+	const [currentPath, setCurrentPath] = useState(DEFAULT_ROUTE);
+
+	useEffect(() => {
+		const routeKey = (Object.keys(ROUTES) as Array<keyof IRoutes>).find(route => {
+			return ROUTES[route].path === location.pathname;
+		});
+
+		let isAvailable = true;
+
+		if (routeKey) {
+			const route = ROUTES[routeKey];
+			isAvailable = route.addon ? Boolean(props.addons[route.addon]) : true;
+		}
+
+		setIsRouteAvailable(isAvailable);
+		setCurrentPath(isAvailable ? location.pathname : DEFAULT_ROUTE);
+	}, [location, props.addons]);
 
 	return(
 		<AppBar position="static">
-			<Tabs value={location.pathname} variant="scrollable" scrollButtons="auto">
+			{!isRouteAvailable && <Redirect to={DEFAULT_ROUTE} />}
+
+			<Tabs value={currentPath} variant="scrollable" scrollButtons="auto">
 				<Tab
 					label="Главная"
 					component={Link}
