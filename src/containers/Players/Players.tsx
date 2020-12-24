e72b43ac9017048f4cb7c5a07daed266ca74d88e
Kirill Ivanov <kirillunlimited@gmail.com>
2020-12-24 21:51:06 +0300

    Add player delete confirm modal

--- src/containers/Players/Players.tsx
+++ src/containers/Players/Players.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import NewPlayer from '../../components/NewPlayer/NewPlayer';
 import {IconButton} from "@material-ui/core";
 import {DeleteForever} from "@material-ui/icons";
@@ -10,6 +10,13 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import styles from "./Players.module.css";
 import Profile from "../../components/Profile/Profile";
+import Tooltip from '@material-ui/core/Tooltip';
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
 
 export default function Players() {
 	const playersContext = useContext(PlayersContext);
@@ -17,8 +24,24 @@ export default function Players() {
 	function onNewPlayerSubmit(name: string) {
 		playersContext.dispatch({type: 'ADD', payload: name});
 	}
-	function onPlayerDelete(name: string) {
+	function deletePlayer(name: string) {
 		playersContext.dispatch({type: 'DELETE', payload: name});
+	}
+
+	const [isDeleteConfirmOpened, setIsDeleteConfirmOpened] = useState(false);
+	const [playerToDelete, setPlayerToDelete] = useState('');
+
+	function handleOpenConfirm (name: string) {
+		setPlayerToDelete(name);
+		setIsDeleteConfirmOpened(true);
+	}
+	function handleDeleteConfirm() {
+		deletePlayer(playerToDelete);
+		handleCloseConfirm();
+	}
+	function handleCloseConfirm() {
+		setPlayerToDelete('');
+		setIsDeleteConfirmOpened(false);
 	}
 
 	return(
@@ -33,8 +56,10 @@ export default function Players() {
 										<Profile name={player.name} />
 									</TableCell>
 									<TableCell className={styles.td}>
-										<IconButton onClick={() => onPlayerDelete(player.name)}>
-											<DeleteForever fontSize="large" color="secondary"/>
+										<IconButton onClick={() => handleOpenConfirm(player.name)}>
+											<Tooltip title="Удалить игрока">
+												<DeleteForever fontSize="large" color="secondary"/>
+											</Tooltip>
 										</IconButton>
 									</TableCell>
 								</TableRow>)
@@ -46,6 +71,23 @@ export default function Players() {
 			}
 
 			{playersContext.state.length === 1 ? <p>Добавьте больше игроков</p> : null}
+
+			<Dialog
+				open={isDeleteConfirmOpened}
+				onClose={handleCloseConfirm}
+				aria-labelledby="alert-dialog-title"
+			>
+				<DialogTitle>Удаление игрока</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Игрок <strong>{playerToDelete}</strong> будет удален, вы уверены?
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCloseConfirm} color="primary">Нет</Button>
+					<Button onClick={handleDeleteConfirm} color="primary" autoFocus>Да</Button>
+				</DialogActions>
+			</Dialog>
 
 			<NewPlayer
 				names={playersContext.state.map(player => player.name)}
