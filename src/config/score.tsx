be7e73180f303721acee0949715ccffa3c961e3d
Kirill Ivanov <kirillunlimited@gmail.com>
2021-02-21 22:18:54 +0300

    Update science icons

--- src/config/score.tsx
+++ src/config/score.tsx
@@ -1,10 +1,12 @@
 import { TScoreKey } from '../types';
-import compass from '../icons/architecture-24px.svg';
-import tablet from '../icons/history_edu-24px.svg';
-import gear from '../icons/settings-24px.svg';
+import compass from '../icons/compass.png';
+import tablet from '../icons/tablet.png';
+import gear from '../icons/gear.png';
+import wildcards from '../icons/wildcard.png';
 
 export const SCORE_ICONS: { [key in TScoreKey]?: string } = {
   compass,
   tablet,
   gear,
+  wildcards,
 };
