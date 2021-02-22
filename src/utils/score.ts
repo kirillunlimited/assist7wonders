8946adda6a464bd16bdb953891026bb96a97f3f9
Kirill Ivanov <kirillunlimited@gmail.com>
2021-02-22 19:38:46 +0300

    Add memoization to science count methods

--- src/utils/score.ts
+++ src/utils/score.ts
@@ -4,6 +4,12 @@ const SCIENCE_KEYS = ['compass', 'tablet', 'gear'] as TScoreKey[];
 const WILDCARD_SCIENCE_KEY = 'wildcards';
 const TREASURY_KEY = 'treasury';
 
+const scienceMemo: {
+  [key: string]: number;
+} = {
+  '0:0:0:0': 0,
+};
+
 export function getTotal(playerScore: IScore): number {
   return getFlatTotal(playerScore) + getScienceTotal(playerScore) + getTreasuryTotal(playerScore);
 }
@@ -11,50 +17,57 @@ export function getTotal(playerScore: IScore): number {
 export function getFlatTotal(playerScore: IScore): number {
   return (Object.keys(playerScore) as Array<TScoreKey>).reduce((sum, key) => {
     if (!SCIENCE_KEYS.includes(key) && key !== WILDCARD_SCIENCE_KEY && key !== TREASURY_KEY) {
-      const value = playerScore[key];
-      sum += value;
+      sum += playerScore[key];
     }
     return sum;
   }, 0);
 }
 
-function getScienceSum(playerScore: IScore): number {
-  let { sum, min } = SCIENCE_KEYS.reduce(
-    ({ sum, min }, key) => {
-      sum += playerScore[key] ** 2;
-      min = Math.min(min, playerScore[key]);
-      return { sum, min };
-    },
-    { sum: 0, min: playerScore[SCIENCE_KEYS[0]] }
-  );
+function getScienceSum(scienceScores: number[]): number {
+  const memoKey: string = [...scienceScores.sort(), 0].join(':');
 
+  if (memoKey in scienceMemo) {
+    return scienceMemo[memoKey];
+  }
+
+  const min = Math.min(...scienceScores);
+  const sum = scienceScores.reduce((sum, score) => sum + score ** 2, 0);
   return sum + min * 7;
 }
 
-function getWildcardPossibilities(playerScore: IScore, wildcards: number): number[] {
-  if (wildcards <= 0) {
-    return [getScienceSum(playerScore)];
+function getWildcardPossibilities(scienceScores: number[], wildcards: number): number {
+  const memoKey: string = [...scienceScores.sort(), wildcards].join(':');
+
+  if (memoKey in scienceMemo) {
+    return scienceMemo[memoKey];
   }
-  return SCIENCE_KEYS.reduce((sum, key) => {
-    const value = playerScore[key];
+
+  if (wildcards <= 0) {
+    scienceMemo[memoKey] = getScienceSum(scienceScores);
+    return scienceMemo[memoKey];
+  }
+
+  const possibilities = scienceScores.reduce((acc, score, index) => {
     return [
-      ...sum,
-      Math.max(
-        ...getWildcardPossibilities(
-          {
-            ...playerScore,
-            [key]: value + 1,
-          },
-          wildcards - 1
-        )
+      ...acc,
+      getWildcardPossibilities(
+        [...scienceScores.slice(0, index), score + 1, ...scienceScores.slice(index + 1)],
+        wildcards - 1
       ),
     ];
   }, [] as number[]);
+
+  scienceMemo[memoKey] = Math.max(...possibilities);
+
+  return scienceMemo[memoKey];
 }
 
 export function getScienceTotal(playerScore: IScore): number {
+  const scienceScores = SCIENCE_KEYS.reduce((scienceScores, key) => {
+    return [...scienceScores, playerScore[key]];
+  }, [] as number[]);
   const wildcards = playerScore[WILDCARD_SCIENCE_KEY];
-  return Math.max(...getWildcardPossibilities(playerScore, wildcards));
+  return getWildcardPossibilities(scienceScores, wildcards);
 }
 
 export function getTreasuryTotal(playerScore: IScore): number {
