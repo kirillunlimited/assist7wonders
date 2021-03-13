2d8936ade63d549c7d71ad3eb7645fb4179b4154
Kirill Ivanov <kirillunlimited@gmail.com>
2021-03-13 18:09:39 +0300

    Reset game separate component

--- src/containers/MainMenu.tsx
+++ src/containers/MainMenu.tsx
@@ -1,22 +1,10 @@
-import React, { useState, useContext } from 'react';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Tooltip,
-  Dialog,
-  DialogActions,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  Button,
-} from '@material-ui/core';
-import { Refresh } from '@material-ui/icons';
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import ResetGame from './ResetGame';
 import AddonsMenu from './AddonsMenu';
 import LanguageMenu from './LanguageMenu';
 import { useTranslation } from 'react-i18next';
-import { GameContext, PlayersContext } from './App';
 import logo from '../img/logo.png';
 import logo2x from '../img/logo2x.png';
 
@@ -33,23 +21,7 @@ const useStyles = makeStyles({
 
 export default function MainMenu() {
   const classes = useStyles();
-  const playersContext = useContext(PlayersContext);
-  const gameContext = useContext(GameContext);
-  const [isConfirmOpened, setIsConfirmOpened] = useState(false);
   const { t } = useTranslation();
-
-  function handleOpenConfirm() {
-    setIsConfirmOpened(true);
-  }
-
-  function handleCloseConfirm() {
-    setIsConfirmOpened(false);
-  }
-
-  function onResetGame() {
-    playersContext.dispatch({ type: 'RESET', payload: gameContext.state });
-    handleCloseConfirm();
-  }
 
   return (
     <AppBar>
@@ -57,30 +29,8 @@ export default function MainMenu() {
         <div className={classes.logoWrapper}>
           <img className={classes.logo} src={logo} alt={t('7wonders')} srcSet={`${logo2x} 2x`} />
         </div>
-        <Tooltip title={t('newGame') as string}>
-          <IconButton onClick={handleOpenConfirm} color="inherit">
-            <Refresh />
-          </IconButton>
-        </Tooltip>
-        <Dialog
-          open={isConfirmOpened}
-          onClose={handleCloseConfirm}
-          aria-labelledby="alert-dialog-title"
-        >
-          <DialogTitle>{t('startNewGame?')}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>{t('startNewGameDescription')}</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseConfirm} color="primary">
-              {t('no')}
-            </Button>
-            <Button onClick={onResetGame} color="primary" autoFocus>
-              {t('yes')}
-            </Button>
-          </DialogActions>
-        </Dialog>
 
+        <ResetGame />
         <AddonsMenu />
         <LanguageMenu />
       </Toolbar>
