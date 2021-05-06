c454d6a1e766f763855a1e73fa1e1eca66b3ad50
Kirill Ivanov <kirillunlimited@gmail.com>
2021-05-06 18:17:53 +0300

    Add Babel addon

---
+++ src/config/addons/babel.ts
@@ -0,0 +1,28 @@
+const babel = {
+  name: 'Babel',
+  maxPlayers: 7,
+  wonders: [],
+  scores: [
+    {
+      id: 'babel',
+      color: '#70809A',
+      counters: [
+        {
+          id: 'babel',
+          min: 0,
+        },
+      ],
+    },
+    {
+      id: 'projects',
+      color: '#9E3F2B',
+      counters: [
+        {
+          id: 'projects',
+        },
+      ],
+    },
+  ],
+};
+
+export default babel;
--- src/config/game.ts
+++ src/config/game.ts
@@ -1,12 +1,13 @@
 import { Game, AddonGame } from '../types';
 import base from './addons/base';
-import cities from './addons/cities';
 import leaders from './addons/leaders';
+import cities from './addons/cities';
+import babel from './addons/babel';
 import armada from './addons/armada';
 import wonderPack from './addons/wonder_pack';
 
 export const BASE_GAME: AddonGame = base;
-export const ADDONS: AddonGame[] = [cities, leaders, armada, wonderPack];
+export const ADDONS: AddonGame[] = [leaders, cities, babel, armada, wonderPack];
 export const GAME_BOILERPLATE: Game = {
   maxPlayers: 0,
   addons: [],
