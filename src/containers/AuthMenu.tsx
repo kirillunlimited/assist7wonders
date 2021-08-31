14658db22bfc90a9d2024106465dc9d8d1bd09ce
Kirill Ivanov <kirillunlimited@gmail.com>
2021-08-31 22:14:23 +0300

    Basic authorization flow

---
+++ src/containers/AuthMenu.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { IconButton, Tooltip, Dialog, DialogTitle, DialogContent } from '@material-ui/core';
+import { Person, ExitToApp } from '@material-ui/icons';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+import { useTranslation } from 'react-i18next';
+import { UserContext } from './App';
+import firebase from '../config/firebase';
+
+export default function AuthMenu() {
+  const userContext = useContext(UserContext);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const { t } = useTranslation();
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        handleCloseModal();
+        return false;
+      },
+    },
+  };
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+      userContext.dispatch({
+        type: 'SET_UID',
+        payload: user?.uid || '',
+      });
+    });
+    return () => unregisterAuthObserver();
+  }, []);
+
+  function handleSignOut() {
+    firebase.auth().signOut();
+  }
+
+  function handleOpenModal() {
+    setIsModalOpened(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpened(false);
+  }
+
+  function getTooltipTitle(): string {
+    return isSignedIn ? t('logout') : t('login');
+  }
+
+  function handleClick() {
+    isSignedIn ? handleSignOut() : handleOpenModal();
+  }
+
+  function renderIcon() {
+    return isSignedIn ? <ExitToApp /> : <Person />;
+  }
+
+  return (
+    <div>
+      <Tooltip title={getTooltipTitle()}>
+        <IconButton onClick={handleClick} color="inherit">
+          {renderIcon()}
+        </IconButton>
+      </Tooltip>
+      <Dialog open={isModalOpened} onClose={handleCloseModal} aria-labelledby="alert-dialog-title">
+        <DialogTitle>{t('login')}</DialogTitle>
+        <DialogContent>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
