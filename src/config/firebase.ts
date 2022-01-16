8e07624b05cd77f7b8d0c83b4a0c23065044072e
Kirill Ivanov <kivanov@ozon.ru>
2022-01-16 18:17:45 +0300

    Separate constants module

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,48 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import { debounce } from 'debounce';
+import { FIREBASE_CONFIG } from './constants';
 
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-};
-
-firebase.initializeApp(config);
-
-const USERS_TABLE = 'users';
-const USER_GAMES_TABLE = 'games';
-
-const getUserRef = (uid: string) => firebase.database().ref(`${USERS_TABLE}/${uid}`);
-
-// TODO: move to common constants
-const SAVE_TIMEOUT = 500;
-
-export async function readUserDataFromDb(uid: string) {
-  if (uid) {
-    try {
-      const ref = getUserRef(uid);
-      const snapshot = await ref.once('value');
-      return snapshot.val();
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  }
-}
-
-// make it debounce
-export const saveGameDataToDb = debounce((uid: string, gameId: number, payload: any) => {
-  if (uid) {
-    if (typeof gameId !== 'number' || gameId === 0) {
-      console.error('🚫 Error while writing data to database --- wrong game id value:', gameId);
-    }
-    /** "update" is better than "set" */
-    getUserRef(uid)
-      .child(`${USER_GAMES_TABLE}/${gameId}`)
-      .update(payload);
-  }
-}, SAVE_TIMEOUT);
+firebase.initializeApp(FIREBASE_CONFIG);
 
 export default firebase;
