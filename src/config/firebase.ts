7da30c6b418c6e2bb0b7caf8a38564ddf8194048
Kirill Ivanov <kivanov@ozon.ru>
2022-01-16 19:33:17 +0300

    Rework analytics init

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,10 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import "firebase/analytics";
 import { FIREBASE_CONFIG } from './constants';
 
 firebase.initializeApp(FIREBASE_CONFIG);
+firebase.analytics();
 
 export default firebase;
