5b5d8a2e9fa434c0c7a7411829d731924be4b7f6
Kirill Ivanov <kivanov@ozon.ru>
2022-01-22 23:37:11 +0300

    Output games history

--- src/reducers/game.ts
+++ src/reducers/game.ts
@@ -22,36 +22,40 @@ type SetGameIdAction= {
 
 export type Action = UpdateAction | SetGameIdAction;
 
+export const updateAction = (gameId: number, payloadAddons: string[]): Game => {
+  const addons = ADDONS.filter(addon => payloadAddons.includes(addon.name));
+  const addonScores = addons.reduce((scores, addon) => {
+    if (addon) {
+      return [...scores, ...addon.scores];
+    }
+    return scores;
+  }, [] as GameScore[]);
+  const addonWonders = addons.reduce((wonders, addon) => {
+    if (addon) {
+      return [...wonders, ...addon.wonders];
+    }
+    return wonders;
+  }, [] as string[]);
+
+  const maxPlayers = [BASE_GAME, ...addons].reduce((max, addon) => {
+    if (addon.maxPlayers > max) {
+      return addon.maxPlayers;
+    }
+    return max;
+  }, 0);
+  return {
+    gameId: gameId,
+    maxPlayers,
+    addons: payloadAddons,
+    wonders: [...BASE_GAME.wonders, ...addonWonders],
+    scores: mergeScores([...BASE_GAME.scores, ...addonScores]),
+  };
+}
+
 const reducer = (state: Game, action: Action) => {
   switch (action.type) {
     case UPDATE:
-      const addons = ADDONS.filter(addon => action.payload.addons.includes(addon.name));
-      const addonScores = addons.reduce((scores, addon) => {
-        if (addon) {
-          return [...scores, ...addon.scores];
-        }
-        return scores;
-      }, [] as GameScore[]);
-      const addonWonders = addons.reduce((wonders, addon) => {
-        if (addon) {
-          return [...wonders, ...addon.wonders];
-        }
-        return wonders;
-      }, [] as string[]);
-
-      const maxPlayers = [BASE_GAME, ...addons].reduce((max, addon) => {
-        if (addon.maxPlayers > max) {
-          return addon.maxPlayers;
-        }
-        return max;
-      }, 0);
-      return {
-        gameId: action.payload.gameId || state.gameId,
-        maxPlayers,
-        addons: action.payload.addons,
-        wonders: [...BASE_GAME.wonders, ...addonWonders],
-        scores: mergeScores([...BASE_GAME.scores, ...addonScores]),
-      };
+      return updateAction(action.payload.gameId || state.gameId, action.payload.addons);
     case SET_GAME_ID:
       return {
         ...state,
