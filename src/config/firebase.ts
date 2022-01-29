528536ec85114a858170be642d1e0520ee4d9b51
Kirill Ivanov <kivanov@ozon.ru>
2022-01-29 22:04:41 +0300

    FIrebase init fallback

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,7 +4,13 @@ import 'firebase/database';
 import "firebase/analytics";
 import { FIREBASE_CONFIG } from './constants';
 
-firebase.initializeApp(FIREBASE_CONFIG);
-firebase.analytics();
+export const isFirebaseOk = Object.values(FIREBASE_CONFIG).every(value => value);
+
+if (isFirebaseOk) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+  firebase.analytics();
+} else {
+  console.warn('⚠️ Firebase init error: configuration is missing some parameters.');
+}
 
 export default firebase;
