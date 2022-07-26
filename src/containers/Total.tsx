4a481282b35874cba0037f7f181efc80d3ef1f60
Kirill Ivanov <kivanov@ozon.ru>
2022-07-26 23:09:05 +0300

    Output games in a single array

--- src/containers/Total.tsx
+++ src/containers/Total.tsx
@@ -3,36 +3,23 @@ import { GamesContext, CurrentGameContext } from './App';
 import { GameParams, GameState } from '../types';
 import Results from '../components/Results';
 import { getGameParamsByGameState } from '../utils/games';
-import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(() => ({
-  history: {
-    marginTop: '24px',
-  },
-  title: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    marginBottom: '8px'
-  },
   historyItem: {
-    marginTop: '8px'
+    "&:not(:first-child)": {
+      marginTop: '8px'
+    },
   }
 }));
 
 export default function Total() {
   const classes = useStyles();
   const gamesContext = useContext(GamesContext);
-  const {currentGameParams, currentGameState, currentGamePlayers} = useContext(CurrentGameContext);
-  const { t } = useTranslation();
+  const {currentGameParams} = useContext(CurrentGameContext);
 
   function getGameParams(game: GameState): GameParams {
     return getGameParamsByGameState(game);
-  }
-
-  function getHistoryGames() {
-    return gamesContext.state.filter(game => game.gameId !== currentGameParams.gameId);
   }
 
   function handleHistoryGameDelete(gameId: number) {
@@ -40,26 +27,21 @@ export default function Total() {
     gamesContext.dispatch({type: 'SET_GAMES', payload: games});
   }
 
+  function sortedGames(): GameState[] {
+    return gamesContext.state.sort((prevGame, nextGame) => nextGame.gameId - prevGame.gameId);
+  }
+
   return (
     <div>
-      <Results
-        players={currentGamePlayers}
-        game={currentGameParams}
-        modified={currentGameState.modified}
-      />
-      {getHistoryGames().length > 0 && <div className={classes.history}>
-        <Typography variant="h2" className={classes.title}>
-          {t('history')}
-        </Typography>
-        {getHistoryGames().map(game => <Results
-          className={classes.historyItem}
-          key={game.gameId}
-          players={game.players}
-          game={getGameParams(game)}
-          modified={game.modified}
-          onDelete={() => handleHistoryGameDelete(game.gameId)}
-        />)}
-      </div>}
+      {sortedGames().map(game => <Results
+        className={classes.historyItem}
+        key={game.gameId}
+        players={game.players}
+        game={getGameParams(game)}
+        modified={game.modified}
+        // The current game is not allowed to be deleted
+        onDelete={currentGameParams.gameId !== game.gameId ? () => handleHistoryGameDelete(game.gameId) : undefined}
+      />)}
     </div>
   );
 }
