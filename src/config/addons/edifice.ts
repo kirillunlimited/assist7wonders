7d2cda19bf5fcd250858edf105e3786e35f7f132
Paolo <polscotman@gmail.com>
2024-05-31 18:25:44 +0200

    add edifice expansion support

---
+++ src/config/addons/edifice.ts
@@ -0,0 +1,18 @@
+const edifice = {
+  name: 'Edifice',
+  maxPlayers: 7,
+  wonders: ['Carthago', 'Ur'],
+  scores: [
+    {
+      id: 'edifices',
+      color: '#f0d162',
+      counters: [
+        {
+          id: 'edifices'
+        },
+      ],
+    },
+  ],
+};
+
+export default edifice;
